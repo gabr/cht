0e8e95545870b3aa06efdc610217b29908b4fc09
gabr <arkadiuszgabrys@gmail.com>
2024-07-10 15:25:38 +0200

    RobinHT - improve find
    and do not copy item if there is no itembuf

--- main.c
+++ main.c
@@ -100,7 +100,9 @@ void RobinHT_Set(char* buf, size_t buflen, size_t itemsiz, ...){
     size_t i,scanned,itemi,dsti;
     assert(buf);
     assert(buflen>0);
+    assert(buflen<(size_t)-1);
     assert(itemsiz>0);
+    assert(itemsiz<=buflen);
     /* extract item data from variadic arguments */
     va_start(args,itemsiz);
     for(i=0;i<itemsiz;i++) itembuf[i]=va_arg(args,int);
@@ -141,15 +143,27 @@ void RobinHT_Set(char* buf, size_t buflen, size_t itemsiz, ...){
 size_t __RobinHT_Find(char* buf, size_t buflen, size_t itemsiz, char* key){
     long itemhash;
     char *dst;
-    size_t i,scanned;
+    long dsthash;
+    size_t i,itemi,dsti,scanned;
     assert(buf);
     assert(buflen>0);
+    assert(buflen<(size_t)-1);
     assert(itemsiz>0);
+    assert(itemsiz<=buflen);
     assert(key);
     itemhash=RobinHT_Hash(key);
-    for(scanned=0,i=itemhash%buflen;;i=(i+1)%buflen){
+    itemi=itemhash%buflen;
+    for(scanned=0,i=itemi;;i=(i+1)%buflen){
         dst=buf+(i*itemsiz);
         if(*dst&&*dst==*key&&!strcmp(dst+1,key+1)) return i;
+        if(*dst) {
+            dsthash=RobinHT_Hash(dst);
+            dsti=dsthash%buflen;
+            /* we should have found our item by now as the current dst one
+               has lover distance to its original index then our item which
+               will never happen */
+            if(i-dsti<i-itemi) return -1;
+        }
         if(++scanned>=buflen) return -1; /* not found */
     }
     assert(0 && "should never reach here");
@@ -160,7 +174,7 @@ size_t __RobinHT_Find(char* buf, size_t buflen, size_t itemsiz, char* key){
 int RobinHT_Get(char* buf, size_t buflen, size_t itemsiz, char* key, char* itembuf){
     size_t i=__RobinHT_Find(buf,buflen,itemsiz,key);
     if(i==(size_t)-1) return 0;
-    memcpy(itembuf,buf+(i*itemsiz),itemsiz);
+    if(itembuf) memcpy(itembuf,buf+(i*itemsiz),itemsiz);
     return 1;
 }
 
