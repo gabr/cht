15327d1d81112075c42da3799604e304ba906ada
gabr <arkadiuszgabrys@gmail.com>
2024-07-10 15:14:24 +0200

    RobinHT - find, get and rm

--- main.c
+++ main.c
@@ -37,8 +37,8 @@ exit
  *  Item i;
  *  Item items[LEN]={}; // remember to initialize your collection to zero
  *  RobinHT_Set(items, LEN, sizeof(Item), (Item){ "New element", 123 });
- *  i = RobinHT_Get(items, LEN, sizeof(Item), "New element");
- *  if (i.key) {
+ *  int found = RobinHT_Get(items, LEN, sizeof(Item), "New element", &i);
+ *  if (found) {
  *    RobinHT_Rem(items, LEN, sizeof(Item), "New element");
  *  }
  *
@@ -50,7 +50,7 @@ exit
  * For convenience and to avoid errors it is advised to wrap that in a macros:
  *
  *  #define ht_set(...) RobinHT_Set(items, LEN, sizeof(Item), (Item){__VA_ARGS__})
- *  #define ht_get(key) RobinHT_Get(items, LEN, sizeof(Item), key)
+ *  #define ht_get(key) RobinHT_Get(items, LEN, sizeof(Item), key, &i), i
  *  #define ht_rem(key) RobinHT_Rem(items, LEN, sizeof(Item), key)
  *  ht_set("New element", 123);
  *  i = ht_get("New element");
@@ -70,7 +70,7 @@ exit
  *
  *  char *hs[LEN]={};
  *  #define hs_set(key) RobinHT_Set(hs, LEN, sizeof(char*), key)
- *  #define hs_has(key) RobinHT_Get(hs, LEN, sizeof(char*), key)
+ *  #define hs_has(key) RobinHT_Get(hs, LEN, sizeof(char*), key, NULL)
  *  #define hs_rem(key) RobinHT_Rem(hs, LEN, sizeof(char*), key)
  *
  *  hs_set("One");
@@ -83,17 +83,17 @@ exit
  */
 
 /* Assumes that the key is char* as first field in the item. */
-long RobinHT_Hash(char* item) {
+long RobinHT_Hash(char* item){
     static const int rol=5;
     unsigned long hash = 0x5555555555555555;
-    while (*item) {
+    while (*item){
         hash^=*item++; /* this below is rol(hash,5) */
         hash =(hash<<rol)|(hash>>(sizeof(hash)-rol));
     }
     return hash;
 }
 
-void RobinHT_Set(char* buf, size_t buflen, size_t itemsiz, ...) {
+void RobinHT_Set(char* buf, size_t buflen, size_t itemsiz, ...){
     va_list args;
     char *item,*dst,*swap,itembuf[itemsiz],swapbuf[itemsiz];
     long itemhash,dsthash;
@@ -105,14 +105,17 @@ void RobinHT_Set(char* buf, size_t buflen, size_t itemsiz, ...) {
     va_start(args,itemsiz);
     for(i=0;i<itemsiz;i++) itembuf[i]=va_arg(args,int);
     va_end(args);
+    assert(itembuf[0]);
     /* calculate hash and find place for the item */
     swap=swapbuf,item=itembuf;
     itemhash=RobinHT_Hash(item);
     itemi=itemhash%buflen;
-    for(scanned=0,i=itemi;;i=(i+1)%buflen) {
+    for(scanned=0,i=itemi;;i=(i+1)%buflen){
         dst=buf+(i*itemsiz);
         /* slot is empty or has the same key */
-        if(*dst==0||(*item==*dst&&!strcmp(item+1,dst+1))) {
+        if(*dst==0||(*item==*dst&&!strcmp(item+1,dst+1))){
+            /* Compare first character before even calling the strcmp().
+               This yelds performance improvements in most cases. */
             memcpy(dst,item,itemsiz);
             return;
         }
@@ -121,7 +124,7 @@ void RobinHT_Set(char* buf, size_t buflen, size_t itemsiz, ...) {
         dsti=dsthash%buflen;
         /* if our item is further from its supposed place then it
          * stays here and we move out the "richer" item */
-        if(i-dsti<i-itemi) {
+        if(i-dsti<i-itemi){
             memcpy(swap,dst,itemsiz);
             memcpy(dst,item,itemsiz);
             dst=item;
@@ -133,6 +136,40 @@ void RobinHT_Set(char* buf, size_t buflen, size_t itemsiz, ...) {
         assert(++scanned<buflen && "no empty slots in the given buf");
     }
     assert(0 && "should never reach here");
+}
+
+size_t __RobinHT_Find(char* buf, size_t buflen, size_t itemsiz, char* key){
+    long itemhash;
+    char *dst;
+    size_t i,scanned;
+    assert(buf);
+    assert(buflen>0);
+    assert(itemsiz>0);
+    assert(key);
+    itemhash=RobinHT_Hash(key);
+    for(scanned=0,i=itemhash%buflen;;i=(i+1)%buflen){
+        dst=buf+(i*itemsiz);
+        if(*dst&&*dst==*key&&!strcmp(dst+1,key+1)) return i;
+        if(++scanned>=buflen) return -1; /* not found */
+    }
+    assert(0 && "should never reach here");
+    return -1;
+}
+
+/* returns 1 if item was found, 0 otherwise */
+int RobinHT_Get(char* buf, size_t buflen, size_t itemsiz, char* key, char* itembuf){
+    size_t i=__RobinHT_Find(buf,buflen,itemsiz,key);
+    if(i==(size_t)-1) return 0;
+    memcpy(itembuf,buf+(i*itemsiz),itemsiz);
+    return 1;
+}
+
+/* returns 1 if item was found and removed, 0 otherwise */
+int RobinHT_Rem(char* buf, size_t buflen, size_t itemsiz, char* key){
+    size_t i = __RobinHT_Find(buf,buflen,itemsiz,key);
+    if(i==(size_t)-1) return 0;
+    buf[i*itemsiz]=0;
+    return 1;
 }
 
 int main(void) {
